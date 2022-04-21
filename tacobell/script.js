e6d0cb971f69937a697886e6f60a96381a7743dd
Frost Sheridan <acrennie@gmail.com>
2022-04-21 11:04:56 -0400

    added taco bell item generator

---
+++ tacobell/script.js
@@ -0,0 +1,44 @@
+var frame_element = document.getElementById("frame");
+var container_element = document.getElementById("item_container");
+var display_element = document.getElementById("item_display");
+
+
+document.body.addEventListener("click", pageClicked);
+frame_element.addEventListener("animationend", animationEnd);
+
+
+function pageClicked()
+{
+	frame_element.style.animation = "frame_rotate 1s linear";
+	container_element.style.animation = "fade_out 0.3s linear forwards";
+}
+
+
+function animationEnd(e)
+{
+	if(e.target.id == "frame")
+	{
+		frame_element.style.animation = "none";
+		display_element.innerHTML = generateItem();
+		container_element.style.animation = "fade_in 0.3s linear";
+	}
+}
+
+
+function generateItem()
+{
+	const modifier = ["Baja Blast", "Volcano", "Ranch", "Chipotle", "Avocado", "White Hot", "Crunchy", "Cheesy", "Doritos", "Nacho", "Cinnamon", "Grilled", "Fiesta", "Classic"];
+	const ingredient = ["Chicken", "Beef", "Steak", "Cheese", "Veggie"];
+	const type = ["Taco", "Burrito", "Nachos", "Bowl", "Crunchwrap", "Freeze", "Quesadilla", "Fries"];
+	
+	var item = ingredient[Math.floor(Math.random() * ingredient.length)] + " " + type[Math.floor(Math.random() * type.length)];
+	if(Math.random() > 0.75) { item += " Supreme"; }
+	
+	var num_modifiers = Math.floor(Math.random() * 3) + 1;
+	for(var i = 0; i < num_modifiers; i++)
+	{
+		item = modifier[Math.floor(Math.random() * modifier.length)] + " " + item;
+	}
+	
+	return item;
+}
